59ff51bcf8ad459e2fc3c2faa7886d10c9f727b5
Mihir Shrestha <121042506+MihiRrr24@users.noreply.github.com>
2025-05-02 20:24:57 +0530

    Updated

---
+++ Binary Search/FindDuplicate.java
@@ -0,0 +1,23 @@
+// https://leetcode.com/problems/find-the-duplicate-number/?envType=problem-list-v2&envId=binary-search
+
+class Solution {
+    public int findDuplicate(int[] nums) {
+        // Phase 1: Detect cycle
+        int slow = nums[0];
+        int fast = nums[0];
+
+        do {
+            slow = nums[slow];
+            fast = nums[nums[fast]];
+        } while (slow != fast);
+
+        // Phase 2: Find entrance to the cycle
+        slow = nums[0];
+        while (slow != fast) {
+            slow = nums[slow];
+            fast = nums[fast];
+        }
+
+        return slow;
+    }
+}

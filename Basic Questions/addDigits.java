e0bd604259c41382d72daf7256fa3a956c77135f
Mihir Shrestha <121042506+MihiRrr24@users.noreply.github.com>
2025-05-28 18:18:19 +0530

    Updated

---
+++ Basic Questions/addDigits.java
@@ -0,0 +1,18 @@
+// https://leetcode.com/problems/add-digits/?envType=problem-list-v2&envId=math
+
+public int addDigits(int num) {
+        int sum=0;
+
+        while(num>=10){
+          sum=0;
+          while(num!=0){
+            int d=num%10;
+            sum+=d;
+            num=num/10;
+          }
+          num=sum;
+          
+        }  
+        return num;
+        
+    }

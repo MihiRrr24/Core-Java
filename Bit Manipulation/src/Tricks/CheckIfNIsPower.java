d816e07b18f9f3e3f61a3162e2926010b86e1844
MihiRrr24 <mihirshrestha4@gmail.com>
2025-08-27 21:43:23 +0530

    IDE Files

--- Bit Manipulation/src/Tricks/CheckIfNIsPower.java
+++ Bit Manipulation/src/Tricks/CheckIfNIsPower.java
@@ -9,7 +9,9 @@ public class CheckIfNIsPower {
 //        if( (n & (n-1)) == 0 ) System.out.println("Yes");
 //        else System.out.println("No");
 
+
         // return n<=0 ? false : (n&(n-1)) == 0;
+
 
         System.out.println(n > 0 && (n & (n - 1)) == 0);
     }

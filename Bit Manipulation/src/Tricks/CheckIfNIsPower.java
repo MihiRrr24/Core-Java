c84e7e9c176dac01ff3d59ffbf19505122610ffd
MihiRrr24 <mihirshrestha4@gmail.com>
2025-08-27 21:39:30 +0530

    IDE Files

--- Bit Manipulation/src/Tricks/CheckIfNIsPower.java
+++ Bit Manipulation/src/Tricks/CheckIfNIsPower.java
@@ -1,10 +1,16 @@
+// https://leetcode.com/problems/power-of-two/
+
 package Tricks;
 
 public class CheckIfNIsPower {
     public static void main(String[] args) {
         int n = 16;
 
-        if( (n & (n-1)) == 0 ) System.out.println("Yes");
-        else System.out.println("No");
+//        if( (n & (n-1)) == 0 ) System.out.println("Yes");
+//        else System.out.println("No");
+
+        // return n<=0 ? false : (n&(n-1)) == 0;
+
+        System.out.println(n > 0 && (n & (n - 1)) == 0);
     }
 }

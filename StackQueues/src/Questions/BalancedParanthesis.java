d66d60339ac2631217a5eb3c85ec73b5f66712d0
MihiRrr24 <mihirshrestha4@gmail.com>
2025-09-15 08:20:28 +0530

    IDE Files

---
+++ StackQueues/src/Questions/BalancedParanthesis.java
@@ -0,0 +1,39 @@
+// https://leetcode.com/problems/valid-parentheses/
+
+package Questions;
+
+import java.util.Stack;
+
+public class BalancedParanthesis {
+
+    public static void main(String[] args) {
+        String s = "()[]{}";
+        System.out.println(isValid(s));
+    }
+
+    public static boolean isValid(String s) {
+        Stack<Character> st = new Stack<>();
+
+        for(int i=0; i<s.length(); i++){
+            if(s.charAt(i) == '(' ||
+                    s.charAt(i) == '{' ||
+                    s.charAt(i) == '[')
+            {
+                st.push(s.charAt(i));
+            }
+
+            else{
+                if(st.isEmpty()) return false;
+                char ch  = st.pop();
+                if(ch == '(' && s.charAt(i) != ')' ||
+                        ch == '{' && s.charAt(i) != '}' ||
+                        ch == '[' && s.charAt(i) != ']')
+                {
+                    return false;
+                }
+            }
+        }
+
+        return st.isEmpty();
+    }
+}

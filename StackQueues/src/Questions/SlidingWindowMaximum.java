a378eac4b9b39fa09e099c6c386c7ca1d40af926
MihiRrr24 <mihirshrestha4@gmail.com>
2025-10-07 15:07:31 +0530

    IDE Files

--- StackQueues/src/Questions/SlidingWindowMaximum.java
+++ StackQueues/src/Questions/SlidingWindowMaximum.java
@@ -3,11 +3,14 @@
 package Questions;
 
 import java.util.ArrayDeque;
+import java.util.Arrays;
 import java.util.Deque;
 
 public class SlidingWindowMaximum {
     public static void main(String[] args) {
-
+        int[] nums = {1,3,-1,-3,5,3,6,7};
+        int k = 3;
+        System.out.println(Arrays.toString(maxSlidingWindow(nums, k)));
     }
 
     public static int[] maxSlidingWindow(int[] nums, int k) {

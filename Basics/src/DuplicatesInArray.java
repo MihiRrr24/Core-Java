027f5b13142cfe92e947819dc6d3b5795d857f84
MihiRrr24 <mihirshrestha4@gmail.com>
2025-10-11 10:30:34 +0530

    IDE Files

--- Basics/src/DuplicatesInArray.java
+++ Basics/src/DuplicatesInArray.java
@@ -10,36 +10,37 @@ public class DuplicatesInArray {
         int n = in.nextInt();
         int[] arr = new int[n];
 
-//        HashMap<Integer, Integer> hash= new HashMap<>();          T.c and S.c = O(N)
-//        for(int i=0; i<arr.length; i++){
-//            arr[i] = in.nextInt();
-//            hash.put(arr[i], hash.getOrDefault(arr[i], 0)+1);
-//        }
-//
-//        int count = 0;
-//        for(int key: hash.keySet()){
-//            if(hash.get(key) > 1){
-//                System.out.print(key+" ");
-//                count++;
-//            }
-//        }
-//        System.out.println(count);
-
-
-        //T>C = O(N2) and S.c = O(1)
-        for(int i=0; i<n; i++){
-            arr[i]=in.nextInt();
+        Map<Integer, Integer> hash= new HashMap<>();       //   T.c and S.c = O(N)
+        for(int i=0; i<arr.length; i++){
+            arr[i] = in.nextInt();
+            hash.put(arr[i], hash.getOrDefault(arr[i], 0)+1);
         }
-        int count=0;
-        for(int i =0; i<n-1; i++){
-            for(int j=i+1; j<n; j++){
-                if(arr[i] == arr[j]){
-                    System.out.print(arr[i]+" ");
-                    count++;
-                }
+
+        for(int num: arr){
+            if(hash.get(num) > 1){
+                System.out.print(num+" ");
             }
         }
         System.out.println();
+
+        int count = (int) hash.keySet().stream().filter(i->hash.get(i)>1).count();
         System.out.println(count);
+
+
+        //T>C = O(N2) and S.c = O(1)
+//        for(int i=0; i<n; i++){
+//            arr[i]=in.nextInt();
+//        }
+//        int count=0;
+//        for(int i =0; i<n-1; i++){
+//            for(int j=i+1; j<n; j++){
+//                if(arr[i] == arr[j]){
+//                    System.out.print(arr[i]+" ");
+//                    count++;
+//                }
+//            }
+//        }
+//        System.out.println();
+//        System.out.println(count);
     }
 }
